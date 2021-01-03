6ff642611fe0e9b3e8e9f77e774f2060fcd18d0e
miamarti10 <miamartigallego@gmail.com>
2021-01-03 10:42:32 +0100

    changes

---
+++ dist/script.dev.js
@@ -0,0 +1,234 @@
+"use strict";
+
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var Song =
+/*#__PURE__*/
+function () {
+  function Song(group, url, tittle, listeners) {
+    _classCallCheck(this, Song);
+
+    this.goup = group;
+    this.url = url;
+    this.tittle = tittle;
+    this.listeners = listeners;
+  }
+
+  _createClass(Song, [{
+    key: "setItemLi",
+    value: function setItemLi() {}
+  }, {
+    key: "setItemGroupName",
+    value: function setItemGroupName(group, url) {}
+  }, {
+    key: "setItemSongTitle",
+    value: function setItemSongTitle(title) {}
+  }, {
+    key: "setListeners",
+    value: function setListeners(listeners) {}
+  }, {
+    key: "getNewElement",
+    value: function getNewElement(group, url, title, listeners) {}
+  }]);
+
+  return Song;
+}();
+
+var loadSongs = function loadSongs() {};
+
+var loadOverview = function loadOverview() {};
+
+var loadTenListened = function loadTenListened() {};
+
+var loadBiggest = function loadBiggest(e) {};
+/* const init = ()=>{
+ 
+    }
+
+
+window.onload = init}
+ */
+
+/*
+1. PONER BOTONES A LAS IMAGENES y CREAR 3 TEXTOS (modo lista ul>li)
+2. DIVIDR LA PAGINA EN 2 BLOQUES Y AÑADIR UL AL BLOQUE DE JAVASCRIPT.
+3. PINTAR CON JAVACRIPT LA LISTA
+4. AÑADIR LISTENERS (BOTONES) A IMAGENES
+5.AÑADIR LISTENERS A TEXTO
+
+
+*/
+//ACCEDIM AL JASON.
+
+
+fetch('./music.json', {}).then(function (response) {
+  return response.json();
+}).then(function (resultat) {
+  return main(resultat);
+});
+var arraySongs = [];
+
+function main(dadaRebuda) {
+  arraySongs = dadaRebuda;
+  listener_estilos_imagenes();
+  listener_estilos_texto();
+}
+
+function printLista(dada) {
+  var listaSongs = document.getElementsByClassName("lista")[0];
+  listaSongs.innerHTML = '';
+
+  for (var i = 0; i < dada.length; i++) {
+    var lista = document.createElement("li");
+    lista.innerHTML = "<a class=\"group-name\"href=\"https://www.last.fm/music/Tame+Impala\">".concat(dada[i].artist.name, "</a><a class=\"song-title\">").concat(dada[i].name, "</a>\n        <div class=\"listeners\">-").concat(dada[i].listeners, "-</div>");
+    listaSongs.appendChild(lista);
+  }
+}
+/* 
+const pintarLista = (lista) =>{
+let musica = new Song();
+
+} */
+
+/* let titulo = document.getElementById("titulo"); */
+
+/* CREACION LISTENERS DE LAS IMAGENES */
+
+
+var listener_estilos_imagenes = function listener_estilos_imagenes() {
+  var botonRock = document.getElementById("rock");
+  botonRock.addEventListener("click", listaRock);
+  var botonHiphop = document.getElementById("hiphop");
+  botonHiphop.addEventListener("click", listaHiphop);
+  var botonJazz = document.getElementById("jazz");
+  botonJazz.addEventListener("click", listaJazz);
+  var botonReggea = document.getElementById("reggea");
+  botonReggea.addEventListener("click", listaReggae);
+};
+/* CREACION LISTENERS DE LAS TEXTO */
+
+
+var listener_estilos_texto = function listener_estilos_texto() {
+  var botonOverview = document.getElementsByClassName("overview")[0];
+  botonOverview.addEventListener("click", listaOverview);
+  var botonTop10 = document.getElementsByClassName("top10")[0];
+  botonTop10.addEventListener("click", listaTop10);
+  /*  let botonBiggest = document.getElementsByClassName("biggest")[0];
+      botonBiggest.addEventListener("click",printLista); */
+};
+/* CAMBIO COLOR DEL TEXTO */
+
+
+function cambioColorTitulo(nombre) {
+  var overview = document.getElementsByClassName("overview")[0];
+  var top10 = document.getElementsByClassName("top10")[0];
+  var biggest = document.getElementsByClassName("biggest")[0];
+  overview.id = "";
+  top10.id = "";
+  biggest.id = "";
+
+  switch (nombre) {
+    case "overview":
+      overview.id = "apretado";
+      break;
+
+    case "top10":
+      top10.id = "apretado";
+      break;
+
+    case "biggest":
+      biggest.id = "apretado";
+      break;
+  }
+}
+/* CREACION DE LA LISTA FILTRADA IMAGENES (4 GENEROS)*/
+
+
+function listaRock() {
+  var arrayRock = [];
+
+  for (var i = 0; i < arraySongs.length; i++) {
+    var musicaRock = arraySongs[i];
+
+    if (musicaRock.genre == 'rock') {
+      arrayRock.push(musicaRock);
+    }
+  }
+
+  printLista(arrayRock);
+}
+
+function listaHiphop() {
+  var arrayHiphop = [];
+
+  for (var i = 0; i < arraySongs.length; i++) {
+    var musciaHiphop = arraySongs[i];
+
+    if (musciaHiphop.genre == "hip-hop") {
+      arrayHiphop.push(musciaHiphop);
+    }
+  }
+
+  printLista(arrayHiphop);
+}
+
+function listaJazz() {
+  var arrayJazz = [];
+
+  for (var i = 0; i < arraySongs.length; i++) {
+    var musciaJazz = arraySongs[i];
+
+    if (musciaJazz.genre == "jazz") {
+      arrayJazz.push(musciaJazz);
+    }
+  }
+
+  printLista(arrayJazz);
+}
+
+function listaReggae() {
+  var arrayReggae = [];
+
+  for (var i = 0; i < arraySongs.length; i++) {
+    var musciaReggae = arraySongs[i];
+
+    if (musciaReggae.genre == "reggae") {
+      arrayReggae.push(musciaReggae);
+    }
+  }
+
+  printLista(arrayReggae);
+}
+/* CREACION DE LA LISTA FILTRADA TEXTO (3 BOTONES)*/
+
+
+function listaOverview() {
+  console.log(arraySongs);
+  printLista(arraySongs);
+}
+
+function listaTop10() {
+  var arrayOrdenado = ordenarMusicaListeners(arraySongs);
+  arrayOrdenado = arrayOrdenado.slice(0, 10);
+  printLista(arrayOrdenado);
+}
+
+function ordenarMusicaListeners(musiques) {
+  var array = _toConsumableArray(musiques);
+
+  return array.sort(function (a, b) {
+    return b.listeners - a.listeners;
+  });
+}
--- script.js
+++ script.js
@@ -1,37 +1,220 @@
 class Song {
-    constructor(){
+    constructor(group, url, tittle, listeners) {
+        this.goup = group;
+        this.url = url;
+        this.tittle = tittle
+        this.listeners = listeners;
     }
 
-    setItemLi(){
-    }
-    setItemGroupName(group,url){
-    }
-    setItemSongTitle(title){
-    }
-    setListeners(listeners){
-    }
-    getNewElement(group,url,title,listeners){
-    }
+    setItemLi() {}
+    setItemGroupName(group, url) {}
+    setItemSongTitle(title) {}
+    setListeners(listeners) {}
+    getNewElement(group, url, title, listeners) {}
 
 }
 
-const loadSongs = ()=>{
+const loadSongs = () => {}
 
-const loadOverview = () =>{
+const loadOverview = () => {}
+
+const loadTenListened = () => {
+
 }
 
-const loadTenListened = ()=>{
- 
+const loadBiggest = (e) => {
+
 }
 
-const loadBiggest = (e)=>{
-   
-}
-
-const init = ()=>{
+/* const init = ()=>{
  
     }
 
 
-window.onload = init;
+window.onload = init}
+ */
 
+/*
+1. PONER BOTONES A LAS IMAGENES y CREAR 3 TEXTOS (modo lista ul>li)
+2. DIVIDR LA PAGINA EN 2 BLOQUES Y AÑADIR UL AL BLOQUE DE JAVASCRIPT.
+3. PINTAR CON JAVACRIPT LA LISTA
+4. AÑADIR LISTENERS (BOTONES) A IMAGENES
+5.AÑADIR LISTENERS A TEXTO
+
+
+*/
+
+
+//ACCEDIM AL JASON.
+
+fetch('./music.json', {})
+    .then(response => response.json())
+    .then((resultat) => main(resultat))
+
+let arraySongs = [];
+
+
+function main(dadaRebuda) {
+    arraySongs = dadaRebuda;
+
+    listener_estilos_imagenes();
+    listener_estilos_texto();
+}
+
+
+function printLista(dada) {
+    let listaSongs = document.getElementsByClassName("lista")[0];
+    listaSongs.innerHTML = '';
+    for (let i = 0; i < dada.length; i++) {
+        let lista = document.createElement("li");
+        lista.innerHTML =
+            `<a class="group-name"href="https://www.last.fm/music/Tame+Impala">${dada[i].artist.name}</a><a class="song-title">${dada[i].name}</a>
+        <div class="listeners">-${dada[i].listeners}-</div>`;
+        listaSongs.appendChild(lista);
+    }
+
+}
+/* 
+const pintarLista = (lista) =>{
+let musica = new Song();
+
+} */
+
+/* let titulo = document.getElementById("titulo"); */
+
+
+/* CREACION LISTENERS DE LAS IMAGENES */
+
+const listener_estilos_imagenes = () => {
+
+    let botonRock = document.getElementById("rock");
+    botonRock.addEventListener("click", listaRock);
+
+
+    let botonHiphop = document.getElementById("hiphop");
+    botonHiphop.addEventListener("click", listaHiphop);
+
+    let botonJazz = document.getElementById("jazz");
+    botonJazz.addEventListener("click", listaJazz);
+
+    let botonReggea = document.getElementById("reggea");
+    botonReggea.addEventListener("click", listaReggae);
+}
+
+/* CREACION LISTENERS DE LAS TEXTO */
+
+const listener_estilos_texto = () => {
+    let botonOverview = document.getElementsByClassName("overview")[0];
+    botonOverview.addEventListener("click", listaOverview);
+
+    let botonTop10 = document.getElementsByClassName("top10")[0];
+    botonTop10.addEventListener("click",listaTop10);
+
+/*  let botonBiggest = document.getElementsByClassName("biggest")[0];
+    botonBiggest.addEventListener("click",printLista); */ 
+}
+
+/* CAMBIO COLOR DEL TEXTO */
+
+function cambioColorTitulo(nombre) {
+    let overview = document.getElementsByClassName("overview")[0];
+    let top10 = document.getElementsByClassName("top10")[0];
+    let biggest = document.getElementsByClassName("biggest")[0];
+    overview.id = "";
+    top10.id = "";
+    biggest.id = "";
+
+    switch (nombre) {
+        case "overview":
+            overview.id = "apretado";
+            break;
+
+        case "top10":
+            top10.id = "apretado";
+            break;
+
+        case "biggest":
+            biggest.id = "apretado";
+            break;
+    }
+
+}
+
+/* CREACION DE LA LISTA FILTRADA IMAGENES (4 GENEROS)*/
+
+
+function listaRock() {
+    let arrayRock = [];
+    for (let i = 0; i < arraySongs.length; i++) {
+        let musicaRock = arraySongs[i];
+        if (musicaRock.genre == 'rock') {
+            arrayRock.push(musicaRock);
+        }
+    }
+    printLista(arrayRock);
+}
+
+function listaHiphop() {
+    let arrayHiphop = [];
+    for (let i = 0; i < arraySongs.length; i++) {
+        let musciaHiphop = arraySongs[i];
+        if (musciaHiphop.genre == "hip-hop") {
+            arrayHiphop.push(musciaHiphop);
+        }
+    }
+    printLista(arrayHiphop);
+
+}
+
+function listaJazz() {
+    let arrayJazz = [];
+
+    for (let i = 0; i < arraySongs.length; i++) {
+        let musciaJazz = arraySongs[i];
+        if (musciaJazz.genre == "jazz") {
+            arrayJazz.push(musciaJazz);
+        }
+    }
+
+    printLista(arrayJazz);
+
+}
+
+function listaReggae() {
+    let arrayReggae = [];
+
+    for (let i = 0; i < arraySongs.length; i++) {
+        let musciaReggae = arraySongs[i];
+        if (musciaReggae.genre == "reggae") {
+            arrayReggae.push(musciaReggae);
+        }
+    }
+
+    printLista(arrayReggae);
+
+}
+
+/* CREACION DE LA LISTA FILTRADA TEXTO (3 BOTONES)*/
+
+function listaOverview() {
+    console.log(arraySongs);
+    printLista(arraySongs);
+    
+
+}
+
+
+function listaTop10() {
+let arrayOrdenado = ordenarMusicaListeners(arraySongs);
+arrayOrdenado = arrayOrdenado.slice(0,10);
+printLista(arrayOrdenado);
+
+}
+
+function ordenarMusicaListeners(musiques) {
+let array=[...musiques]
+    return array.sort((a, b) => {
+        
+    return b.listeners -  a.listeners 
+    });
+}
